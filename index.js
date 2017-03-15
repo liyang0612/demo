95de45719ce8c55880fbde664db5e29f47162ec6
码疯 <liyang.box0612@gmail.com>
2017-03-15 11:45:20 +0800

    delete
    delete function

--- index.js
+++ index.js
@@ -4,23 +4,7 @@ window.onload = function() {
     d();
 }
 
-function a() {
-    console.log("aa")
-}
 
-function b() {
-	console.log("b")
-}
-
-function c() {
-		console.log("c")
-}
-function d() {
-	console.log("d")
-}
-function e(){
-	console.log("c")
-}
 //Created by liyang ,2017/03/13
 function name() {
 	console.log("liyang")
